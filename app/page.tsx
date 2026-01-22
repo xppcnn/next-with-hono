f972236664097f7c78b8ab935cbc190651e1a972
xppcnn <xppcnn@gmail.com>
2026-01-22 09:57:23 +0800

    根路径redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-'use client'
-import { useEffect, useState } from 'react'
-import { useAuth } from "@/hooks/useAuth"  
-export default function Home() {
-  const [message, setMessage] = useState()
-
-  const { user } =   useAuth()
-  if (!user) return <p>Loading...</p>
-
-  return <p className='text-2xl text-red-500'>{user.name}</p>
+import { redirect } from 'next/navigation'
+import { headers } from "next/headers";
+import { useAuth } from "@/hooks/useAuth"
+import { auth } from "@/lib/auth"
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+  if (session?.user) return redirect("/dashboard")
+  if (!session?.user) return redirect("/sign-in")
+  return <p>Loading...</p>
 }
